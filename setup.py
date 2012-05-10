8120b68aef8dbd3b53c2e869c85e3206c7a420eb
Joseph McCullough <joseph@vertstudios.com>
2012-05-10 12:30:26 -0500

    Added setup.py file

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python
+from setuptools import setup
+import os, shutil
 
-from distutils.core import setup
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-    from distutils.command.build_py import build_py
-
-setup(
-    name='pyopenkeyval',
-    version='0.4',
-    description='pyopenkeyval: A simple interface to OpenKeyval.org',
-    long_description='A simple, dict-like interface to read and write values on the OpenKeyval service.',
-    author='Marcus Ekelund',
-    author_email='marcus.ekelund@gmail.com',
-    url='http://github.com/marcuse/pyopenkeyval',
-    py_modules=['pyopenkeyval'],
-    license="Public domain",
-    platforms=["any"],
-    cmdclass={'build_py': build_py},
-)
+setup(name='OpenKeyval CLI',
+      version='1.0',
+      description='Command line interface for PyOpenKeyval',
+      author='Joseph McCullough',
+      author_email='joseph@vertstudios.com',
+      packages = ['pyopenkeyval'],
+      scripts=['okv']
+     )
